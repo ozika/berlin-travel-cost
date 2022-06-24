c29505f1693e67374a01d9a7093729f8872063a7
Ondrej Zika <zika.ondra@gmail.com>
2022-06-24 23:26:16 +0200

    added few bits

--- scripts/berlin_transport_app.py
+++ scripts/berlin_transport_app.py
@@ -44,7 +44,8 @@ donate_string = '''<form action="https://www.paypal.com/donate" method="post" ta
 <input type="hidden" name="hosted_button_id" value="3X5CKVFVU723L" />
 <input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
 <img alt="" border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1" />
-</form>'''
+</form>
+'''
 
 
 
