b9b9653254d774a9523e736876aee91292eeb362
Ondrej Zika <zika.ondra@gmail.com>
2022-06-25 20:04:33 +0200

    split into quick and extensive

--- scripts/berlin_transport_app.py
+++ scripts/berlin_transport_app.py
@@ -5,92 +5,130 @@ import pandas as pd
 import numpy as np
 import os
 
-
-
 #dispval = False
 sharenow_deals_km_price = 0.19
 miles_parking_rate = 0.29
 
-st.header("Carsharing price calculator @ Berlin")
-st.markdown("This site compares mobility sharing companies (MILES, SHARE NOW) for better deal given your estimated distance, duration, parking etc.")
-st.markdown("It also checks for any packages/deals that one can take. Enjoy! :heart:")
-
-showresults = True#st.button("Show me!")
-
-st.sidebar.markdown("**1. Input the estimated distance and duration for the ride (google estimates will do)**")
-distance = st.sidebar.number_input("Estimated distance (in km)", value=0.0, step=0.1, help="Distance in km")
-unit = st.sidebar.selectbox("Select unit", ["min", "hr", "day"])
-estdur = st.sidebar.number_input("Estimated duration (in "+unit+")", value=0 ,step=1, help="Duration in "+unit)
-if unit == "hr":
-    estdur = estdur * 60
-elif unit == "day":
-    estdur = estdur * 60 * 24
-
-st.sidebar.markdown("Parking is ON TOP if the driving time")
-parking = st.sidebar.number_input("Parking time (in "+unit+")", value=0)
-if unit == "hr":
-    parking = parking * 60
-elif unit == "day":
-    parking = parking * 60 * 24
-
-st.sidebar.markdown("**2. Select companies you want to compare**")
-options = st.sidebar.multiselect(
-     'Select:',
-     ['MILES', 'SHARENOW'])
-
-
-#st.text(os.path.join(root_folder, "rates", "miles.csv"))
 df_miles = pd.read_csv(os.path.join(root_folder, "rates", "miles.csv"))
 df_miles["duration_min"] = df_miles["duration"] * 60
 df_sn = pd.read_csv(os.path.join(root_folder, "rates", "sharenow.csv"))
 df_sn["duration_min"] = df_sn["duration hrs"] * 60
 
+st.header("Carsharing price calculator @ Berlin")
+st.markdown("This site compares mobility sharing companies (MILES, SHARE NOW) for better deal given your estimated distance, duration, parking etc. Enjoy! :heart:")
+#st.markdown("**Quick version**: You just need to type in distance and duration, all else is optional.")
+#st.markdown("**Extensive version**: Needs more input, but also checks for packages/deals specific to a given car type.")
+#st.markdown("---")
+showresults = True#st.button("Show me!")
+
+st.sidebar.markdown("## 1. Choose comparison type")
+scope = st.sidebar.radio("Quick = based on time, distance and parking only\nExtensive = also deals, car type, better for long-term", ["Quick", "Extensive"], horizontal=True)
+
+if scope=="Quick":
+    st.sidebar.markdown("## 2. Type in distance, duration and parking time")
+    distance = st.sidebar.number_input("Estimated distance (in km)", value=0, step=1, help="Distance in km")
+    unit = "min"
+    estdur = st.sidebar.number_input("Estimated duration (in "+unit+")", value=0 ,step=1, help="Duration in "+unit)
+    parking = st.sidebar.number_input("Parking time (in "+unit+")", value=0)
+
+    if (distance != 0.0) & (estdur != 0):
+         st.sidebar.markdown("## 3. Select companies you want to compare")
+         options = st.sidebar.multiselect(
+              'You can specify the rates below if you need to.',
+              ['MILES', 'SHARENOW'], default=['MILES', 'SHARENOW'])
+         if "MILES" in options:
+             st.sidebar.markdown("### **Miles**")
+             miles_kmrate = st.sidebar.number_input("Rate per km (in EUR)", value=0.89, step=0.01, help="For example: 0.89")
+             miles_unlockfee = 1
+             can_refuel_miles = st.sidebar.checkbox("Will refuel Miles (5 EUR cashback)", key="refuel1", value=False)
+             miles_cost = round(miles_kmrate*distance + miles_unlockfee + can_refuel_miles*(-5) + parking*miles_parking_rate,2)
+             #st.markdown("**MILES cost:** "+str(miles_cost))
+             st.metric(label="", value="MILES: "+str(miles_cost)+" EUR")
+
+             st.markdown("Breakdown: "+str(miles_kmrate)+" EUR/km * "+str(distance) +" km + "+str(miles_unlockfee)+" (unlock fee) - "+str(can_refuel_miles*(5))  + " (refuel) + "+str(miles_parking_rate) + " EUR/min * "+str(parking)+" (parking)"  )
+
+         if "SHARENOW" in options:
+             estdur = estdur + parking #in sharenow one pays for the entire time
+             st.sidebar.markdown("### **ShareNow**")
+             sharenow_minrate = st.sidebar.number_input("Rate per minute (in EUR)\nInclude discount.", key="sn_minrate_1", value=0.26 ,step=0.01, help="For example: 0.26")
+             sharenow_unlockfee = 0
+             can_refuel_sharenow = st.sidebar.checkbox("Will refuel Sharenow (5 EUR cashback)", key="refuelsn1", value=False)
+             if (sharenow_minrate != 0.00) & showresults:
+                 sharenow_cost = round(sharenow_minrate*estdur + can_refuel_sharenow*(-5),2)
+                 #st.markdown("**SHARE NOW cost:** "+str(sharenow_cost))
+                 st.metric(label="", value="SHARE NOW: "+str(sharenow_cost)+" EUR")
+                 st.markdown("Breakdown: "+str(sharenow_minrate)+" EUR/min * "+str(estdur)+" min - " +str(can_refuel_sharenow*(5))+" (refuel)" )
+                 st.markdown("> **NOTE:** *The `Quick` option doesn't check for packages/deals! Use the `Extensive` version for that.*")
+elif scope=="Extensive":
+    st.sidebar.markdown("## 2. Type in distance, duration and parking time")
+    st.sidebar.markdown("(google estimates will do)")
+    distance = st.sidebar.number_input("Estimated distance (in km)", value=0, step=1, help="Distance in km")
+    unit = st.sidebar.selectbox("Select unit", ["min", "hr", "day"])
+    estdur = st.sidebar.number_input("Estimated duration (in "+unit+")", value=0 ,step=1, help="Duration in "+unit)
+    if unit == "hr":
+        estdur = estdur * 60
+    elif unit == "day":
+        estdur = estdur * 60 * 24
+
+    #st.sidebar.markdown("")
+    parking = st.sidebar.number_input("Parking time (in "+unit+")", value=0, help="Parking is on top of the driving time")
+    if unit == "hr":
+        parking = parking * 60
+    elif unit == "day":
+        parking = parking * 60 * 24
+
+    st.sidebar.markdown("**2. Select companies you want to compare**")
+    options = st.sidebar.multiselect(
+         'Select:',
+         ['MILES', 'SHARENOW'], default=['MILES', 'SHARENOW'])
+
+    if ("MILES" in options) & (distance != 0) & (estdur != 0):
+        st.sidebar.markdown("### **Miles**")
+        miles_unlockfee = 1
+        miles_car = st.sidebar.selectbox("Car type", ["S", "M",	"L", "Premium"])
+        miles_dicounted = st.sidebar.checkbox("Discounted car", key="m_disc_1", value=False)
+        if miles_dicounted:
+            miles_kmrate = df_miles["discounted_price"].loc[df_miles["cartype"].isin([miles_car])].unique()[0] # t.sidebar.number_input("Rate per km (in EUR)", value=0.89, step=0.01, help="For example: 0.89")
+        else:
+            miles_kmrate = df_miles["base_price"].loc[df_miles["cartype"].isin([miles_car])].unique()[0] # t.sidebar.number_input("Rate per km (in EUR)", value=0.89, step=0.01, help="For example: 0.89")
+        st.sidebar.markdown("Rate per km: **"+str(miles_kmrate)+"**")
+        can_refuel_miles = st.sidebar.checkbox("Will refuel Miles (5 EUR cashback)", key="refuel2", value=False)
+        if (miles_kmrate != 0.0) & showresults:
+            miles_cost = round(miles_kmrate*distance + miles_unlockfee + can_refuel_miles*(-5) + parking*miles_parking_rate,2)
+            #st.markdown("**MILES cost:** "+str(miles_cost))
+            st.metric(label="", value="MILES: "+str(miles_cost)+" EUR")
+            st.markdown("Breakdown: "+str(miles_kmrate)+" EUR/km * "+str(distance) +" km + "+str(miles_unlockfee)+" (unlock fee) - "+str(can_refuel_miles*(5))  + " (refuel) + "+str(miles_parking_rate) + " EUR/min * "+str(parking)+" (parking)"  )
+
+            id = ((df_miles["duration_min"]>estdur) & (df_miles["distance"]>distance) & (df_miles["cost"]<miles_cost))
+            if sum(id)>0:
+                st.markdown("You can save money with the following MILES packages:")
+                st.dataframe(df_miles.loc[id,:])
+
+    if ("SHARENOW" in options) & (distance != 0) & (estdur != 0):
+        estdur = estdur + parking #in sharenow one pays for the entire time
+
+        st.sidebar.markdown("### **ShareNow**")
+        sharenow_minrate = st.sidebar.number_input("Rate per minute (in EUR)\nInclude discount.", key="sn_minrate_2", value=0.26 ,step=0.01, help="For example: 0.29")
+        sharenow_car = st.sidebar.selectbox("Car type", ["Fiat 500", "BMW 1 Series",	"Peugeot 308",	"Citroen C3",	"Fiat 500X",	"MINI Countryman",	"MINI 3-door",
+                                                                     "MINI Convertable",	"MINI 5-door",	"MINI Clubman",	"Peugeot 208",	"Peugeot 3008",	"BMW Convertible",	"BMW Active Tourer",	"BMW X1",	"BMW X2"])
+        df_sn["Total cost (with km)"] = df_sn[sharenow_car] + sharenow_deals_km_price * distance
+        sharenow_unlockfee = 0
+        can_refuel_sharenow = st.sidebar.checkbox("Will refuel Sharenow (5 EUR cashback)", value=False)
+        if (sharenow_minrate != 0.00) & showresults:
+            sharenow_cost = round(sharenow_minrate*estdur + can_refuel_sharenow*(-5),2)
+            #st.markdown("**SHARE NOW cost:** "+str(sharenow_cost))
+            st.metric(label="", value="SHARE NOW: "+str(sharenow_cost)+" EUR")
+            st.markdown("Breakdown: "+str(sharenow_minrate)+" EUR/min * "+str(estdur)+" min - " +str(can_refuel_sharenow*(5))+" (refuel)" )
+            df_sn_2 = df_sn.loc[:,["duration_min"]+[sharenow_car]]
+            id = ((df_sn_2["duration_min"]>estdur) & ( (df_sn_2[sharenow_car] + sharenow_deals_km_price*distance) < sharenow_cost))
+            if sum(id)>0:
+                st.markdown("You can save money with the following ShareNow packages:")
+                st.dataframe(df_sn.loc[id,["duration"]+[sharenow_car]+["Total cost (with km)"]])
+
 donate_string = '''<a href="https://www.paypal.com/donate/?hosted_button_id=3X5CKVFVU723L">
 <img src="https://github.com/ozika/berlin-travel-cost/blob/main/img/donate.png?raw=true" width="60px">
 </a>
 '''
-
-
-
-
-if "MILES" in options:
-
-    st.sidebar.markdown("**Miles**")
-    miles_kmrate = st.sidebar.number_input("Rate per km (in EUR)", value=0.00, step=0.01, help="For example: 0.89")
-    miles_unlockfee = 1
-    can_refuel_miles = st.sidebar.checkbox("Will refuel Miles (5 EUR cashback)", value=False)
-    if (miles_kmrate != 0.0) & showresults:
-        miles_cost = miles_kmrate*distance + miles_unlockfee + can_refuel_miles*(-5) + parking*miles_parking_rate
-        st.markdown("**MILES cost:** "+str(miles_cost))
-        st.markdown("Breakdown: "+str(miles_kmrate)+" EUR/km * "+str(distance) +" km + "+str(miles_unlockfee)+" (unlock fee) - "+str(can_refuel_miles*(5))  + " (refuel) + "+str(miles_parking_rate) + " EUR/min * "+str(parking)+" (parking)"  )
-
-        id = ((df_miles["duration_min"]>estdur) & (df_miles["distance"]>distance) & (df_miles["cost"]<miles_cost))
-        if sum(id)>0:
-            st.markdown("You can save money with the following MILES packages:")
-            st.dataframe(df_miles.loc[id,:])
-
-if "SHARENOW" in options:
-    estdur = estdur + parking #in sharenow one pays for the entire time
-
-    st.sidebar.markdown("**ShareNow**")
-    sharenow_minrate = st.sidebar.number_input("Rate per minute (in EUR)\nInclude discount.", value=0.00 ,step=0.01, help="For example: 0.29")
-    #st.sidebar.markdown()
-    sharenow_car = st.sidebar.selectbox("Car type (optional, only necessary if you want to check ShareNow packages)", ["Fiat 500", "BMW 1 Series",	"Peugeot 308",	"Citroen C3",	"Fiat 500X",	"MINI Countryman",	"MINI 3-door",
-                                                                 "MINI Convertable",	"MINI 5-door",	"MINI Clubman",	"Peugeot 208",	"Peugeot 3008",	"BMW Convertible",	"BMW Active Tourer",	"BMW X1",	"BMW X2"])
-    df_sn["Total cost (with km)"] = df_sn[sharenow_car] + sharenow_deals_km_price * distance
-    sharenow_unlockfee = 0
-    can_refuel_sharenow = st.sidebar.checkbox("Will refuel Sharenow (5 EUR cashback)", value=False)
-    if (sharenow_minrate != 0.00) & showresults:
-        sharenow_cost = sharenow_minrate*estdur + can_refuel_sharenow*(-5)
-        st.markdown("**SHARE NOW cost:** "+str(sharenow_cost))
-        st.markdown("Breakdown: "+str(sharenow_minrate)+" EUR/min * "+str(estdur)+" min - " +str(can_refuel_sharenow*(5))+" (refuel)" )
-        df_sn_2 = df_sn.loc[:,["duration_min"]+[sharenow_car]]
-        id = ((df_sn_2["duration_min"]>estdur) & ( (df_sn_2[sharenow_car] + sharenow_deals_km_price*distance) < sharenow_cost))
-        if sum(id)>0:
-            st.markdown("You can save money with the following ShareNow packages:")
-            st.dataframe(df_sn.loc[id,["duration"]+[sharenow_car]+["Total cost (with km)"]])
-
-
 st.markdown("---")
 st.markdown('For improvements, suggestions and any errors you can [write me here](https://github.com/ozika/berlin-travel-cost/issues) (just click "open issue")')
 st.markdown("If this saved you some money, please tip me. I love working on this but it also takes some of my time (and a lot of B-lin coffee :coffee:) Berlin :heart:")
