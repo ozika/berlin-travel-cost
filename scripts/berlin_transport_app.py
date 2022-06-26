7ca767eb4a6b6c428b3b701d46813206a429bd7f
Ondrej Zika <zika.ondra@gmail.com>
2022-06-26 10:47:20 +0200

    fix

--- scripts/berlin_transport_app.py
+++ scripts/berlin_transport_app.py
@@ -4,10 +4,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-#import pathlib
-#from bs4 import BeautifulSoup
-#import logging
-#import shutil
+import pathlib
+from bs4 import BeautifulSoup
+import logging
+import shutil
 
 #dispval = False
 sharenow_deals_km_price = 0.19
@@ -184,4 +184,34 @@ st.markdown(donate_string, unsafe_allow_html=True)
 #st.markdown(twitter_string, unsafe_allow_html=True)
 
 
-#inject_ga()
+
+def inject_ga():
+    GA_ID = "google_analytics"
+    GA_JS = """
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-8RSB8MRRGF"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-8RSB8MRRGF');
+    </script>
+    """
+
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):  # if cannot find tag
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # recover from backup
+        else:
+            shutil.copy(index_path, bck_index)  # keep a backup
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
+
+inject_ga()
